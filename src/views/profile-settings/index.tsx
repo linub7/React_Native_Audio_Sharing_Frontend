f1444df897b471b7db09c457b1ab6e29497fcc64
a1a2a3a4a5 <linub7@gmail.com>
2023-06-08 20:00:54 +0430

    custom header for profile settings screen -> done

--- src/views/profile-settings/index.tsx
+++ src/views/profile-settings/index.tsx
@@ -1,3 +1,4 @@
+import AppHeader from '@components/profile/app-header';
 import {FC} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
@@ -6,13 +7,16 @@ interface Props {}
 const ProfileSettingsScreen: FC<Props> = props => {
   return (
     <View style={styles.container}>
+      <AppHeader title="Profile > Settings" />
       <Text style={{color: 'white'}}>PRofile settings screen</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
 });
 
 export default ProfileSettingsScreen;
